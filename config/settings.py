186ee3f7c2387ce1793c27fc5a94878e179bd23c
Tony <toshik06rom@rambler.ru>
2025-06-26 01:44:10 +0300

    add models. 1, 2 task in homework

--- config/settings.py
+++ config/settings.py
@@ -41,8 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'rest_framework',
-    "****",
-    "*****",
+    'lms',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -129,11 +129,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = "data/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "data/media")
 
-AUTH_USER_MODEL = "***.*****"
+AUTH_USER_MODEL = "users.User"
 
-LOGIN_URL = "***:*****"
-LOGIN_REDIRECT_URL = "/***/*****/"
-LOGOUT_REDIRECT_URL = "/***/*****/"
+# LOGIN_URL = "***:*****"
+# LOGIN_REDIRECT_URL = "/***/*****/"
+# LOGOUT_REDIRECT_URL = "/***/*****/"
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,4 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("***/", include("****.urls", namespace="***")),
-    path("****/", include("****.urls", namespace="****")),
-    path("*****/", include("*****.urls", namespace="*****")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
